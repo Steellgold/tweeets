30ff4814a31da47cc84defe38124a3c01cc67c43
Gaëtan H <steellgold0@gmail.com>
2023-08-07 02:27:19 +0200

    feat(ui): add Slider component feat(ui): add ThemeProvider component feat(ui): add Toggle component feat(configs): add WritingSentiment, WritingStyle, WritingTone types feat(utils): add stripe utility function

---
+++ src/lib/configs/generation/types.ts
@@ -0,0 +1,42 @@
+export type WritingSentiment =
+  "sentiment-neutral" |
+  "sentiment-joyful" |
+  "sentiment-sad" |
+  "sentiment-energetic" |
+  "sentiment-calm" |
+  "sentiment-confident" |
+  "sentiment-hopeful" |
+  "sentiment-sarcastic" |
+  "sentiment-playful" |
+  "sentiment-thoughtful" |
+  "sentiment-motivational" |
+  "sentiment-appreciative" |
+  "sentiment-curious" |
+  "sentiment-exhilarated" |
+  "sentiment-grateful" |
+  "sentiment-adventurous";
+
+export type WritingStyle = "style-neutral" |
+ "style-friendly" |
+ "style-formal" |
+ "style-humorous" |
+ "style-informative" |
+ "style-inspirational" |
+ "style-educational" |
+ "style-poetic" |
+ "style-controversial" |
+ "style-sentimental" |
+ "style-mysterious" |
+ "style-engaging" |
+ "style-narrative";
+
+export type WritingTone = "tone-neutral" |
+ "tone-optimistic" |
+ "tone-pessimistic" |
+ "tone-angry" |
+ "tone-joyful" |
+ "tone-sad" |
+ "tone-energetic" |
+"tone-appreciative" |
+ "tone-curious" |
+ "tone-motivational";
