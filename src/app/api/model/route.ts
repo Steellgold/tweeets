901f243f434017f557e5ce9e3c9a68c0dad1126b
Gaëtan H <steellgold0@gmail.com>
2023-08-11 22:22:43 +0200

    fix(route.ts): change DELETE function parameter from `params` to `request` feat(route.ts): add validation for `id` parameter in DELETE function using zod schema

--- src/app/api/model/route.ts
+++ src/app/api/model/route.ts
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 import { prisma } from "@/lib/utils/database";
 import { ModelResponseSchema } from "@/lib/utils/schemas";
 import dayjs from "dayjs";
+import { z } from "zod";
 
 export async function POST(request: NextRequest): Promise<NextResponse> {
   const supabase = createRouteHandlerClient({ cookies });
@@ -57,18 +58,24 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   return NextResponse.json(model, { status: 200 });
 }
 
-export async function DELETE(params: { id: string }): Promise<NextResponse> {
+export async function DELETE(request: NextRequest): Promise<NextResponse> {
   const supabase = createRouteHandlerClient({ cookies });
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) return NextResponse.json({}, { status: 401 });
 
+  const schema = z.object({
+    id: z.string()
+  }).safeParse(await request.json());
+
+  if (!schema.success) return NextResponse.json(schema.error, { status: 400 });
+
   const model = await prisma.model.findUnique({
-    where: { id: params.id },
+    where: { id: schema.data.id },
     include: { user: false }
   });
 
   if (!model) return NextResponse.json({}, { status: 404 });
   if (model.userId !== user.id) return NextResponse.json({}, { status: 401 });
-  await prisma.model.delete({ where: { id: params.id } });
+  await prisma.model.delete({ where: { id: model.id } });
   return NextResponse.json({}, { status: 204 });
 }
