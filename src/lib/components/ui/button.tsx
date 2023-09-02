2cbc6170a30cc1bf47d1577ed83849310954bf52
Gaëtan H <steellgold0@gmail.com>
2023-09-03 01:47:17 +0200

    feat(ui/button.tsx): add new button variant 'star' refactor(types.test.ts): remove unused test case for pro feature selection

--- src/lib/components/ui/button.tsx
+++ src/lib/components/ui/button.tsx
@@ -18,7 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        twitter: "bg-blue-500 text-white hover:bg-blue-600"
+        twitter: "bg-blue-500 text-white hover:bg-blue-600",
+        star: "bg-yellow-500 text-white hover:bg-yellow-600",
       },
       size: {
         default: "h-10 px-4 py-2",
--- src/lib/configs/generation/types.test.ts
+++
@@ -1,8 +0,0 @@
-import { it, expect } from "vitest";
-import { testProSelected } from "./types";
-
-it("test if without pro, selected pro features pass or no", () => {
-  expect(testProSelected(false, ["sentiment-calm", "style-poetic", "tone-optimistic"])).toBe(false);
-  expect(testProSelected(false, ["sentiment-calm", "tone-optimistic", "tone-motivational"])).toBe(false);
-  expect(testProSelected(false, ["sentiment-calm", "tone-optimistic", "sentiment-energetic"])).toBe(true);
-});
