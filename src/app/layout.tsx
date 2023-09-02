e3d3f221af94d2d83b4ad378fe39f8eeca3eae89
Gaëtan H <steellgold0@gmail.com>
2023-09-02 13:06:02 +0200

    chore(privacy): remove privacy policy page component chore(tos): remove terms of service page component chore(api): remove reset cron route chore(api): remove model route chore(api): remove model share route
    chore(route.ts): remove unused route file for user/fpdone
    chore(route.ts): remove unused route file for user
    style(layout.tsx): update body background color in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import { Navbar } from "@/lib/components/navbar/navbar";
 import { UserProvider } from "@/lib/contexts/UserProvider";
 import { ThemeProvider } from "@/lib/components/ui/theme-provider";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 export { metadata } from "@/lib/configs/metadata";
 
@@ -17,7 +16,7 @@ const os = Open_Sans({ subsets: ["latin"] });
 const RootLayout: Component<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("bg-white dark:bg-zinc-950", os)}>
+      <body className={cn("bg-white dark:bg-[#090909]", os)}>
         <Analytics />
 
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
@@ -25,15 +24,6 @@ const RootLayout: Component<PropsWithChildren> = ({ children }) => {
             <Navbar />
             {children}
           </UserProvider>
-
-          <footer className="flex flex-wrap gap-4 items-center justify-center w-full h-24 dark:border-zinc-850">
-            <Link href={"/privacy"} className="text-muted hover:text-muted-foreground">
-              Privacy Policy
-            </Link>
-            <Link href={"/tos"} className="text-muted hover:text-muted-foreground">
-              Terms of Service
-            </Link>
-          </footer>
         </ThemeProvider>
       </body>
     </html>
