b6d3e383b39a21d2ca0344193c32af0026295be1
Gaëtan H <steellgold0@gmail.com>
2023-09-28 16:35:30 +0200

    fix(card.tsx): import 'cn' function from utils module feat(card.tsx): add author information to BlogCard component fix(card.tsx): remove border color from 'blogPost' badge variant in Badge component

--- src/app/blog/card.tsx
+++ src/app/blog/card.tsx
@@ -1,22 +1,23 @@
 import { Badge } from "@/lib/components/ui/badge";
 import CardSpotlight from "@/lib/components/ui/card-spotlight";
-import { intify } from "@/lib/utils";
+import { cn, intify } from "@/lib/utils";
 import type { Prisma } from "@prisma/client";
 import dayjs from "dayjs";
 import { AlarmClock, Dot, Glasses, MessagesSquare, Pin, Sparkles, Wand2 } from "lucide-react";
 import Link from "next/link";
 import type { ReactElement } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/lib/components/ui/avatar";
 
 type BlogPostProps = Prisma.PostsGetPayload<{
   include: {
-    author: {include: { posts: false }};
+    author: { include: { posts: false } };
     tags: true;
     categories: true;
     comments: true;
   };
 }>;
 
-const BlogCard = ({ comments, isPinned, slug, title, coverUrl, views, tags, excerpt, publishedAt }: BlogPostProps): ReactElement => {
+const BlogCard = ({ comments, isPinned, slug, title, coverUrl, views, tags, excerpt, publishedAt, author }: BlogPostProps): ReactElement => {
   const testTime = (): boolean => {
     return dayjs(publishedAt).isAfter(dayjs());
   };
@@ -36,19 +37,30 @@ const BlogCard = ({ comments, isPinned, slug, title, coverUrl, views, tags, exce
               </div>
             )}
           </div>
-          {tags && tags.map((tag) => (
-            <Badge
-              key={tag.id}
-              variant={"blogPost"}>
-              {tag.id == "ai" && <><Sparkles size={18} className="text-yellow-300" />&nbsp;<span className="text-yellow-300">{tag.name}</span></>}
-              {tag.id == "release" && <>
-                <Wand2 size={18} className="text-emerald-300" />&nbsp;
-                <span className="text-emerald-300">{tag.name}</span>
-              </>}
-            </Badge>
-          ))}
+          {tags && tags.length > 0 && (
+            <div className="flex gap-2 items-center">
+              {tags && tags.slice(0, 2).map((tag) => (
+                <Badge
+                  key={tag.id}
+                  variant={"blogPost"}
+                  className={cn({
+                    "border-emerald-300 text-emerald-300": tag.id == "release",
+                    "border-yellow-300 text-yellow-300": tag.id == "ai"
+                  })}>
+                  {tag.id == "ai" && <><Sparkles size={18} />&nbsp;<span>{tag.name}</span></>}
+                  {tag.id == "release" && <><Wand2 size={18} />&nbsp;<span>{tag.name}</span></>}
+                </Badge>
+              ))}
+              {tags && tags.length > 2 && <span className="text-zinc-500 text-xs">+{tags.length - 2}</span>}
+            </div>
+          )}
         </div>
-        <Title title={title} slug={slug} isPinned={isPinned} comments={(comments ?? []).length} views={views ?? 0} excerpt={excerpt} />
+        <Title
+          title={title}
+          author={{ username: author.username, avatarUrl: author.pictureUrl || null }}
+          comments={(comments ?? []).length}
+          views={views ?? 0}
+          excerpt={excerpt} />
       </CardSpotlight>
     </Link>
   );
@@ -56,25 +68,31 @@ const BlogCard = ({ comments, isPinned, slug, title, coverUrl, views, tags, exce
 
 type BlogCardTitleProps = {
   title: string;
-  slug: string;
-  isPinned: boolean;
   excerpt: string;
   comments: number;
   views: number;
+  author: {
+    username: string;
+    avatarUrl: string | null;
+  };
 };
 
-const Title = ({ title, comments, views, excerpt }: BlogCardTitleProps): ReactElement => {
+const Title = ({ title, comments, views, excerpt, author  }: BlogCardTitleProps): ReactElement => {
   return (
     <div className="absolute bottom-4 flex flex-col gap-2">
       <div className="flex flex-col">
-        <span className="flex gap-2 font-medium items-center">
-          {title}
-        </span>
-        <span className="flex gap-2 items-center text-sm text-zinc-400 line-clamp-2">
-          {excerpt}
-        </span>
+        <span className="flex gap-2 font-medium items-center text-sm md:text-base">{title}</span>
+        <span className="flex gap-2 items-center text-xs md:text-sm text-zinc-400 line-clamp-2">{excerpt}</span>
       </div>
       <div className="flex gap-2 text-sm text-zinc-400 items-center">
+        <span className="flex gap-2 items-center">
+          <Avatar className="w-5 h-5">
+            <AvatarImage src={author.avatarUrl ?? "/images/placeholder.png"} />
+            <AvatarFallback>{author.username.slice(0, 2)}</AvatarFallback>
+          </Avatar>
+          {author.username}
+        </span>
+        <Dot size={16} className="text-zinc-500" />
         <span className="flex gap-1 items-center">
           <MessagesSquare size={18} className="text-zinc-500" />&nbsp;
           {comments} comments
