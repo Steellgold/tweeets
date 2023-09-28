9c1795de7c0fb2329ff41fcd12d2d713d2ddc169
Gaëtan H <steellgold0@gmail.com>
2023-09-28 18:14:39 +0200

    fix(card.tsx): fix condition for rendering Pin component refactor(cards.tsx): refactor BlogPosts component to include loading state and add styling to the header section

--- src/app/blog/cards.tsx
+++ src/app/blog/cards.tsx
@@ -5,6 +5,7 @@ import type { Prisma } from "@prisma/client";
 import type { ReactElement } from "react";
 import useSWR from "swr";
 import BlogCard from "./card";
+import { cn } from "@/lib/utils";
 
 type BlogPosts = Prisma.PostsGetPayload<{
   include: {
@@ -21,16 +22,34 @@ type BlogPosts = Prisma.PostsGetPayload<{
 
 const BlogPosts = (): ReactElement => {
   const { data, isLoading } = useSWR<BlogPosts[]>(`${process.env.NEXT_PUBLIC_URL ?? "https://tweeets.app"}/api/blog`, fetcher);
-  if (isLoading) return <></>;
 
   return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {data && data.map((post) => (
-          <BlogCard key={post.id} {...post} />
-        ))}
+    <>
+      <div className="mx-auto flex flex-col items-center justify-center max-w-screen-2xl" suppressHydrationWarning>
+        <div className="flex flex-col items-center justify-center mb-10">
+          <div className="flex flex-col items-center w-full px-4">
+            <h1 className={cn(
+              "text-2xl font-bold tracking-tighter sm:text-5xl xl:text-6xl bg-clip-text text-transparent",
+              "bg-gradient-to-r from-zinc-500 via-zinc-300 to-zinc-500 h-full text-center py-3"
+            )}>
+              Tweeets Blog
+            </h1>
+
+            <p className="md:text-xl text-zinc-400 mx-auto text-center">
+              Read about the latest updates and news about Tweeets.
+            </p>
+          </div>
+        </div>
+
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {!isLoading && data && data.map((post) => (
+              <BlogCard key={post.id} {...post} />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
