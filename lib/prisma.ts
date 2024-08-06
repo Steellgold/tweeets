ab4c691c088fb93073d8253be3cf76cdc1dac04f
Gaëtan H <gaetanhus@gmail.com>
2024-08-06 22:27:54 +0200

    feat(prisma): add Prisma client singleton function to manage a single instance feat(prisma): create Prisma schema file with User, Account, and Session models

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+export function prismaClientSingleton(): PrismaClient {
+  return new PrismaClient();
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export { prisma };
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
