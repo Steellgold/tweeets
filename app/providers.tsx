f50b79b3a6ab4a47f1a84417b11eaf9974df32a4
Gaëtan H <gaetanhus@gmail.com>
2024-08-07 22:18:31 +0200

    feat(providers.tsx): add SessionProvider from next-auth/react to enable session management in the app

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,6 +5,7 @@ import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { SessionProvider } from "next-auth/react";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -15,8 +16,10 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-    </NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
