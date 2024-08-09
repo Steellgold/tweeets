764c9272e32007c9dafbbe92620640480aa57e70
Gaëtan H <gaetanhus@gmail.com>
2024-08-09 22:56:54 +0200

    feat: Add onborda/dist to the list of files to be processed by Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/onborda/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
