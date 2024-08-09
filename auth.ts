37636c9ae156d5c1f786cb78f1e20eb6743743eb
Gaëtan H <gaetanhus@gmail.com>
2024-08-09 22:56:38 +0200

    feat(auth.ts): update Twitter provider configuration

--- auth.ts
+++ auth.ts
@@ -7,5 +7,5 @@ import { prisma } from "./lib/prisma"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // @ts-ignore
   adapter: PrismaAdapter(prisma),
-  providers: [ Twitter ],
+  providers: [Twitter]
 })
