d26b81ee74dffde95835f51f1720b771664bd1bf
Gaëtan H <gaetanhus@gmail.com>
2024-08-10 17:14:25 +0200

    refactor: Remove unnecessary columns and add credits column to User model

---
+++ prisma/migrations/20240810133407_credits/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - The required column `id` was added to the `Session` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt";
+
+-- AlterTable
+ALTER TABLE "Session" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "id" TEXT NOT NULL,
+ADD CONSTRAINT "Session_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "credits" INTEGER NOT NULL DEFAULT 5,
+ADD COLUMN     "emailVerified" TIMESTAMP(3);
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
