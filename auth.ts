6e75e6900622c7ba2e05383f25a06ca2c49a73f9
Gaëtan H <gaetanhus@gmail.com>
2024-08-10 17:13:47 +0200

    refactor: Update auth.ts to include Twitter provider and session callback

--- auth.ts
+++ auth.ts
@@ -1,11 +1,34 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import NextAuth from "next-auth"
 import Twitter from "next-auth/providers/twitter"
+import NextAuth, { type DefaultSession } from "next-auth"
 
 import { prisma } from "./lib/prisma"
- 
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      credits: number
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    credits: number;
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // @ts-ignore
   adapter: PrismaAdapter(prisma),
-  providers: [Twitter]
+  providers: [Twitter],
+  callbacks: {
+    async session({ session, user}) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          credits: user.credits
+        }
+      }
+    }
+  }
 })
