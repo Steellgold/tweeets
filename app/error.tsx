2ba45d0e31f5f2af37e7ceaceb2a9b1d46712888
Gaëtan H <gaetanhus@gmail.com>
2024-08-11 20:52:26 +0200

    feat(error.tsx): add Button component from @nextui-org/button library for better UI design feat(error.tsx): add RefreshCcw icon from lucide-react library for visual appeal feat(error.tsx): add useState hook to manage button click state for better user experience feat(error.tsx): add handleTryAgain function to handle button click event and provide feedback feat(error.tsx): update UI to display error message and provide a button to retry action

--- app/error.tsx
+++ app/error.tsx
@@ -1,31 +1,47 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button } from "@nextui-org/button";
+import { RefreshCcw } from "lucide-react";
 
-export default function Error({
-  error,
-  reset,
-}: {
+import { Component } from "@/components/component";
+import { cn } from "@/lib/utils";
+
+type ErrorProps = {
   error: Error;
   reset: () => void;
-}) {
+};
+
+const Error: Component<ErrorProps> = ({ error, reset }) => {
   useEffect(() => {
-    // Log the error to an error reporting service
     /* eslint-disable no-console */
     console.error(error);
   }, [error]);
 
+  const [clicked, setClicked] = useState(false);
+
+  const handleTryAgain = async () => {
+    setClicked(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setClicked(false);
+  };
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
+    <div className="flex flex-col items-center justify-center h-full gap-1">
+      <h2 className="text-2xl font-bold">Something went wrong!</h2>
+      <span className="text-gray-500 mb-3">An error occurred while processing your request.</span>
+      <Button
+        color="danger"
+        variant="flat"
+        onClick={() => handleTryAgain()}
       >
+        <RefreshCcw className={cn({
+          "animate-spin": clicked,
+        })} size={16} />
         Try again
-      </button>
+      </Button>
     </div>
   );
 }
+
+export default Error;
