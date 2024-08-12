eeac334e535dea8a8e27505bd558ea88a550cb70
Gaëtan H <gaetanhus@gmail.com>
2024-08-12 02:37:54 +0200

    chore(onboarding.provider.tsx): remove unused Zap icon import to clean up code feat(onboarding.provider.tsx): add support for generating a thread instead of a single tweet feat(onboarding.provider.tsx): add option to select the length of the tweet feat(onboarding.provider.tsx): add options for adding emojis and indicators to tweets for engagement feat(onboarding.provider.tsx): update cost details for tweet generation, including thread mode pricing feat(onboarding.provider.tsx): update final step message for tweet generation process

--- lib/providers/onboarding.provider.tsx
+++ lib/providers/onboarding.provider.tsx
@@ -2,7 +2,6 @@
 
 import { Onborda, OnbordaProvider, Step } from "onborda";
 import { PropsWithChildren, ReactElement } from "react";
-import { Zap } from "lucide-react";
 import { Code } from "@nextui-org/code";
 
 import { OnboardCard } from "@/components/OnboardCard";
@@ -25,34 +24,23 @@ export const OnboardingProvider: Component<PropsWithChildren> = ({ children }: P
       <Onborda cardComponent={OnboardCard} shadowOpacity="0.8" shadowRgb="18,18,18" steps={[
         config("✏️", "What's your tweet about?", "What is the desired position, give us a context we can work with, like for example \"France, World Cup 2018\"", "#onborda-step1"),
         config("🎭", "What's the tone of your tweet?", "Select the tone of your tweet, it can be humoristic, serious, informative or normal.", "#onborda-step2"),
-        config("⚡", "How fast do you want the generator to be?", (
-          <div className="flex flex-col gap-2">
-            <p>Select the speed of the generator, it can be normal or fast.</p>
-
-            <div className="flex flex-wrap gap-2 items-center mt-3">
-              <Code className="text-model3"><Zap color="#FFC107" fill="#FFC107" size={18} /></Code>
-              <span>Normal mode is powered by <Code className="text-model3">GPT-3.5 Turbo</Code></span>              
-            </div>
-
-            <div className="flex flex-wrap gap-2 items-center">
-              <Code className="text-model4"><Zap color="#17c562" fill="#17c562" size={18} /></Code>
-              <span>Fast mode is powered by <Code className="text-model4">GPT-4o mini</Code></span>
-            </div>
-          </div>
-        ), "#onborda-step3"),
+        config("🧵", "Do you want to generate a thread?", "You can generate a thread instead of a single tweet, just enable the checkbox below.", "#onborda-step3"),
         config("📏", "How long do you want your tweet to be?", "Select the length of your tweet, it can be 140, 280 or more characters if you have a Twitter Premium account.", "#onborda-step4"),
+
+        config("⚙️", "Options", (
+          <div className="flex flex-col gap-2">
+            <p className="mb-0.5">Emojies: We will add emojis to your tweets to make them more engaging.</p>
+            <p>Enable indicators: We will add indicators to your tweets to make it clear that it&apos;s a thread (e.g. 1/3).</p>
+          </div>
+        ), "#onborda-step5"),
+
         config("💳", "How much does a tweet cost?", (
           <div className="flex flex-col gap-2">
-            <p>Each tweet generation will cost you 1 credit, and for a length above 280 characters it will cost you 2 credits.</p>
-            <p>Each credit costs <Code>$0.99</Code>, but you can buy 10 credits for <Code>$5.99</Code>.</p>
-
-            <div className="flex flex-wrap gap-2 items-center mt-2">
-              <Code className="text-model4"><Zap color="#17c562" fill="#17c562" size={18} /></Code>
-              <span>Fast mode costs one extra credit.</span>
-            </div>
+            <p>• Single tweets will cost <Code>1 credit each</Code>.</p>
+            <p>• On thread mode, the five first tweets will <Code>cost 1 credit each</Code>, and the following tweets will cost <Code>1 credit less</Code>.</p>
           </div>
-        ), "#onborda-step5"), 
-        config("🚀", "You're ready to tweet!", "You're ready to tweet, click on the button below to generate your tweet.", "#onborda-step6"),
+        ), "#onborda-step6"),        
+        config("🚀", "You're ready to tweet!", "You're ready to tweet, click on the button below to generate your tweet.", "#onborda-step7"),
       ]}>
         {children}
       </Onborda>
