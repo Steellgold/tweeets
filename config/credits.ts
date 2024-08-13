f687ef95bb6eb3a6579b8e477ae677220a2978db
Gaëtan H <gaetanhus@gmail.com>
2024-08-14 00:34:43 +0200

    feat(credits.ts): add getTokensCount function to calculate the number of tokens required based on character count, thread status, and thread count

--- config/credits.ts
+++ config/credits.ts
@@ -5,3 +5,13 @@ export const calculateCredits = (threadLength: number): number => {
     return threadLength - 1;
   }
 }
+
+export const getTokensCount = (charCount: number, isThread: boolean, threadCount?: number): number => {
+  const tokensForSingleTweet = Math.ceil(charCount * 2);
+
+  if (isThread) {
+    return Math.min(tokensForSingleTweet * (threadCount || 2), 10181);
+  } else {
+    return Math.min(tokensForSingleTweet, 10181);
+  }
+}
