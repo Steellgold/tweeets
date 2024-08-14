a7747bc520c407735fd48830dd9ac263dc65f1ad
Gaëtan H <gaetanhus@gmail.com>
2024-08-14 03:09:44 +0200

    feat(error.tsx): add reset function call to clear error state after handling error in Error component

--- app/error.tsx
+++ app/error.tsx
@@ -24,6 +24,7 @@ const Error: Component<ErrorProps> = ({ error, reset }) => {
     setClicked(true);
     await new Promise((resolve) => setTimeout(resolve, 1000));
     setClicked(false);
+    reset();
   };
 
   return (
