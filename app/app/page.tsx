ea201f4337c7f8d5829e0120968960550486b5fe
Gaëtan H <gaetanhus@gmail.com>
2024-08-16 13:10:34 +0200

    fix(page.tsx): update onClick event handler to pass "demo" parameter to startOnborda function

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -160,7 +160,7 @@ const Page = () => {
 
         <CardFooter className="flex justify-between flex gap-2">
           <div className="flex gap-2">
-            <Button color="default" size="sm" onClick={startOnborda}>
+            <Button color="default" size="sm" onClick={() => startOnborda("demo")}>
               <Presentation size={16} />
               Onboarding
             </Button>
